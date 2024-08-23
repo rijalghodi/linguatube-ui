7cc4610cd759036dfe3fb8fa455693bdb8951175
Rijal Ghodi <rijalghodi10@gmail.com>
2024-08-23 17:09:53 +0700

    fix: import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,8 @@
-import { AppLayout, LandingPageLayout } from "@/components/layouts";
-import {
-  Box,
-  Button,
-  Container,
-  Group,
-  Modal,
-  Stack,
-  Text,
-  TextInput,
-  Title,
-  UnstyledButton,
-} from "@mantine/core";
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import logo from "@/public/logo.png";
-import demoImage from "@/public/og_image.png";
-import { useDisclosure } from "@mantine/hooks";
-import Link from "next/link";
-import { IconBubble, IconMessageCircle } from "@tabler/icons-react";
-import { OpenaiApiKeyInput } from "@/components";
-import { useSearchParams } from "next/navigation";
-import { Introduction } from "@/components/Introduction";
-import { useRouter } from "next/router";
 import { AppWorkspace } from "@/components/AppWorkspace";
+import { Introduction } from "@/components/Introduction";
+import { AppLayout } from "@/components/layouts";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
